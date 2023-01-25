44edc339ec7020d64fbb59fa996c0b4ef1c163d4
hlitio <hernanlitio@gmail.com>
2023-01-24 21:35:21 -0300

    fix login

--- src/App.js
+++ src/App.js
@@ -16,10 +16,20 @@ mutation($correo: String!, $password: String!, $idperfil: Int!)
 }}
 `
 
-const Agregar_usuario = ()=>{
+
+const LOGIN_USUARIO = gql`
+mutation($correo: String!, $password: String!){
+  login(correo: $correo, password: $password) {
+    token
+  }
+}
+`
+
+
+const Agregar_Usuario = ()=>{
   const[nuevoUsuario,{ data, loading, error }] = useMutation(AGREGAR_USUARIO)
-  if (loading) return 'Submitting...';
-  if (error) return `Submission error! ${error.message}`;
+  //if (loading) return 'Submitting...';
+  //if (error) return `Submission error! ${error.message}`;
   let correo,password,idperfil
   return(
     <div className="contenedor-login">
@@ -27,20 +37,20 @@ const Agregar_usuario = ()=>{
       <form onSubmit={async e=>{
         e.preventDefault()
         let passencryp = await bcryptjs.hash(password.value,8)
-        nuevoUsuario({variables:{correo: correo.value,password: passencryp,idperfil:parseInt(idperfil.value,10)}})
+        nuevoUsuario({variables:{correo: correo.value,password: password.value,idperfil:parseInt(idperfil.value,10)}})
       }} className="pure-form pure-form-aligned">
          <fieldset>
           <legend>Alta de usuarios</legend>
           <div className= "pure-control-group">
-            <label for= "correo">Correo: </label>
+            <label htmlFor= "correo">Correo: </label>
             <input ref={node =>{correo=node}} id="correo"></input>
           </div>
           <div className= "pure-control-group">
-            <label for= "password">Password: </label>
+            <label htmlFor= "password">Password: </label>
             <input ref={node =>{password=node} } type="password" id="password"></input>
           </div>
           <div className= "pure-control-group">
-            <label for= "idperfil">IdPerfil: </label>
+            <label htmlFor= "idperfil">IdPerfil: </label>
             <input ref={node=>{idperfil=node}} id="idperfil"></input>
           </div>
           <div className= "pure-controls">
@@ -55,12 +65,19 @@ const Agregar_usuario = ()=>{
 }
 
 const Login = ()=>{
+
+  const [loginUser,{ data, loading, error }] = useMutation(LOGIN_USUARIO)
+
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
  
+
+
   let correo,password
-  const secret = new TextEncoder().encode(
-    'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
-  )
-  const alg = 'HS256'
+  //const secret = new TextEncoder().encode(
+  //  'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
+  //)
+  //const alg = 'HS256'
 
   return(
     <div className="contenedor-login">
@@ -70,29 +87,29 @@ const Login = ()=>{
         let passencryp = await bcryptjs.hash(password.value,8)
 
 
-        const jwt = await new jose.SignJWT({ user:123456 })
-        .setProtectedHeader({ alg })
-        .setIssuedAt()
-        .setIssuer('urn:example:issuer')
-        .setAudience('urn:example:audience')
-        .setExpirationTime('2h')
-        .sign(secret)
+        //const jwt = await new jose.SignJWT({ user:123456 })
+        //.setProtectedHeader({ alg })
+        //.setIssuedAt()
+        //.setIssuer('urn:example:issuer')
+        //.setAudience('urn:example:audience')
+        //.setExpirationTime('2h')
+        //.sign(secret)
 
-        console.log("Token:", jwt)
-        console.log("----------------------")
-        console.log("Verificando")
-        console.log("----------------------")
+        //console.log("Token:", jwt)
 
-        const { payload, protectedHeader } = await jose.jwtVerify(jwt, secret, {
-          issuer: 'urn:example:issuer',
-          audience: 'urn:example:audience',
-        })
+
+
+        //const { payload, protectedHeader } = await jose.jwtVerify(jwt, secret, {
+        //  issuer: 'urn:example:issuer',
+        //  audience: 'urn:example:audience',
+        //})
         
-        console.log(protectedHeader)
-        console.log(payload)
+        //console.log(protectedHeader)
+        //console.log(payload)
 
+        loginUser({variables:{correo: correo.value,password:passencryp}})
+        console.log(data.usuario)
 
-        
       }} className="pure-form pure-form-aligned">
          <fieldset>
           <legend>Login</legend>
@@ -117,6 +134,7 @@ const Login = ()=>{
 }
 
 
+
 const client = new ApolloClient({
   uri:"http://localhost:4000/graphql"
 })
@@ -130,8 +148,9 @@ function App() {
     <ApolloProvider client={client}>
       <div className="App">
       
-        
+        <Agregar_Usuario />
         <Login />
+        
 
       </div>
     </ApolloProvider>
